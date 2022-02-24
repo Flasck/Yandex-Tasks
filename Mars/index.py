9e7440b8c3e5a93eda3559ac35dadf3d46b7ebaa
Leamich <mishak2005med@mail.ru>
2022-02-24 19:15:12 +0400

    1st task

--- Mars/index.py
+++ Mars/index.py
@@ -4,8 +4,12 @@ import flask
 app = flask.Flask(__name__)
 
 
-@app.route('/')
+@app.route('/<t>')
 @app.route('/index/<t>')
 def index(t):
     user = "Ученик Яндекс.Лицея"
     return flask.render_template('base.html', title=t, username=user)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
